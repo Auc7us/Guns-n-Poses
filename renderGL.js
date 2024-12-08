5838e3e6446a52b6c7ffb88b2ffa872f902d37ac
auc7us <keshavsharan97@gmail.com>
2024-12-08 17:00:35 -0600

    fixed light direction to align to make physical sense and align with corrected normals

--- renderGL.js
+++ renderGL.js
@@ -14,7 +14,6 @@ export function renderScene(gl, program1, program2, worldObjects, camera, projec
     mat4.lookAt(viewMatrix, camera.position, camera.target, camera.up);
     mat4.perspective(projectionMatrix, projection.fov, projection.aspect, projection.near, projection.far);
 
-
     // Program 1 Setup #########################################################################
     gl.useProgram(program1);
     const locations = getLocations(gl, program1);
@@ -23,13 +22,11 @@ export function renderScene(gl, program1, program2, worldObjects, camera, projec
     gl.uniformMatrix4fv(locations.uniforms.projectionMatrix, false, projectionMatrix);
     // const light = {direction: vec3.normalize([], [1, 2, -2]), color: [1, 1, 1]};
     const light = {direction: vec3.create(), color: [1, 1, 1]};
-    // loopTime = -3.14/2;
-    const loopTimeEnv = loopTime * 0.1;
-    // vec3.normalize(light.direction, [0.1 * Math.sin(loopTimeEnv)*Math.sin(loopTimeEnv), 1*Math.cos(loopTimeEnv)*Math.cos(loopTimeEnv), -0.2*Math.sin(loopTimeEnv)*Math.sin(loopTimeEnv)]);
-    vec3.normalize(light.direction, [0.1 , 1, -0.8]);
+    // vec3.normalize(light.direction, [1 , 0, 0]);
+    vec3.normalize(light.direction, [0.1 , -1, -0.8]);
     /*LIGHT DIRECTION REMEMBERRR
     X Positive = Left to Right (light is on left)
-    Y Positive = Top to bottom (light is on top)
+    Y Negative = Top to bottom (light is on top)
     Z Negative = Front to back (light is behind you at the start)
     */
     gl.uniform3fv(locations.uniforms.lightDirection, light.direction);
@@ -82,8 +79,8 @@ export function renderScene(gl, program1, program2, worldObjects, camera, projec
     // gl.uniform3fv(locations2.uniforms.viewPosition, camera.position);
     // Set light for program2
     const light2 = {direction: vec3.create(), color: [1, 1, 1]};
-    
-    vec3.normalize(light2.direction, [Math.sin(loopTime), Math.cos(loopTime), Math.sin(loopTime)]);
+    // loopTime = 0;
+    vec3.normalize(light2.direction, [Math.sin(loopTime), -Math.cos(loopTime), Math.sin(loopTime)]);
     gl.uniform3fv(locations2.uniforms.lightDirection, light2.direction);
     gl.uniform3fv(locations2.uniforms.lightColor, light2.color);
     // Render gun and bullet with specular effects
