0302263eee5ca80f9c0e53780599aaedcf756687
auc7us <keshavsharan97@gmail.com>
2024-12-08 17:05:35 -0600

    [Tested] Removed redundant code in program2 setup, fixed stair positions, added specular highlights to gun

--- renderGL.js
+++ renderGL.js
@@ -59,10 +59,10 @@ export function renderScene(gl, program1, program2, worldObjects, camera, projec
     drawRepeatingObj(gl, worldObjects.surface, locations, 0, -3999, 1000, [4000, 0, -16000], { angle: 0, axis: [0, 1, 0] }, [1, -1, 1]);
     // Render stairs
     gl.uniform3fv(locations.uniforms.objectColor, [0.4, 0.4, 0.4]);
-    placeObj(gl, worldObjects.surface, [4000, 1000,  -4000], { angle: -Math.PI / 2, axis: [1, 0, 0] }, [1, -1, 1], locations, 0);
-    placeObj(gl, worldObjects.surface, [4000, 2000,  -7000], { angle: -Math.PI / 2, axis: [1, 0, 0] }, [1, -1, 1], locations, 0);
-    placeObj(gl, worldObjects.surface, [4000, 3000, -10000], { angle: -Math.PI / 2, axis: [1, 0, 0] }, [1, -1, 1], locations, 0);
-    placeObj(gl, worldObjects.surface, [4000, 4000, -13000], { angle: -Math.PI / 2, axis: [1, 0, 0] }, [1, -1, 1], locations, 0);
+    placeObj(gl, worldObjects.surface, [4000,    0,  -4000], { angle: -Math.PI / 2, axis: [1, 0, 0] }, [1, -1, 1], locations, 0);
+    placeObj(gl, worldObjects.surface, [4000, 1000,  -7000], { angle: -Math.PI / 2, axis: [1, 0, 0] }, [1, -1, 1], locations, 0);
+    placeObj(gl, worldObjects.surface, [4000, 2000, -10000], { angle: -Math.PI / 2, axis: [1, 0, 0] }, [1, -1, 1], locations, 0);
+    placeObj(gl, worldObjects.surface, [4000, 3000, -13000], { angle: -Math.PI / 2, axis: [1, 0, 0] }, [1, -1, 1], locations, 0);
     drawRepeatingObj(gl, worldObjects.surface, locations, 0, -2999, 1000, [4000, 1000,  -4000], { angle: 0, axis: [0, 1, 0] }, [1, -1, 1]);
     drawRepeatingObj(gl, worldObjects.surface, locations, 0, -2999, 1000, [4000, 2000,  -7000], { angle: 0, axis: [0, 1, 0] }, [1, -1, 1]);
     drawRepeatingObj(gl, worldObjects.surface, locations, 0, -2999, 1000, [4000, 3000, -10000], { angle: 0, axis: [0, 1, 0] }, [1, -1, 1]);
@@ -75,8 +75,7 @@ export function renderScene(gl, program1, program2, worldObjects, camera, projec
     // Set uniform matrices for program2
     gl.uniformMatrix4fv(locations2.uniforms.viewMatrix, false, viewMatrix);
     gl.uniformMatrix4fv(locations2.uniforms.projectionMatrix, false, projectionMatrix);
-    // console.log("Camera position:", camera.position);
-    // gl.uniform3fv(locations2.uniforms.viewPosition, camera.position);
+    gl.uniform3fv(locations2.uniforms.viewPosition, camera.position);
     // Set light for program2
     const light2 = {direction: vec3.create(), color: [1, 1, 1]};
     // loopTime = 0;
@@ -86,26 +85,32 @@ export function renderScene(gl, program1, program2, worldObjects, camera, projec
     // Render gun and bullet with specular effects
     loopTime = loopTime*3
     const objectsToRenderWithProgram2 = [
-        // { obj: worldObjects.gun, translation: [2000, 1600, 4350], color: [0.8, 0.7, 0.5], scale: [1, -1, 1]},
-        { obj: worldObjects.gun,     translation: [2120, 1770, 4550], rotation: { angle:                    0, axis: [0, 1, 0] }, scale: [1, -1, 1], color: [0.83, 0.67, 0.3]},
+        { obj: worldObjects.gun,     translation: [2120, 1770, 4550], rotation: { angle:                    0, axis: [0, 1, 0] }, scale: [1, -1, 1], color: [0.83, 0.67, 0.3]},    
         { obj: worldObjects.fMuzzle, translation: [2120, 1770, 4550], rotation: { angle:             loopTime, axis: [0, 0, 1] }, scale: [1, -1, 1], color: [0.83, 0.67, 0.3]},
-        { obj: worldObjects.fMuzzle, translation: [2120, 1770, 4550], rotation: { angle:     loopTime+Math.PI, axis: [0, 0, 1] }, scale: [1, -1, 1], color: [0.83, 0.67, 0.3]},
         { obj: worldObjects.fMuzzle, translation: [2120, 1770, 4550], rotation: { angle:   loopTime+Math.PI/4, axis: [0, 0, 1] }, scale: [1, -1, 1], color: [0.83, 0.67, 0.3]},
         { obj: worldObjects.fMuzzle, translation: [2120, 1770, 4550], rotation: { angle:   loopTime+Math.PI/2, axis: [0, 0, 1] }, scale: [1, -1, 1], color: [0.83, 0.67, 0.3]},
         { obj: worldObjects.fMuzzle, translation: [2120, 1770, 4550], rotation: { angle: loopTime+3*Math.PI/4, axis: [0, 0, 1] }, scale: [1, -1, 1], color: [0.83, 0.67, 0.3]},
+        { obj: worldObjects.fMuzzle, translation: [2120, 1770, 4550], rotation: { angle:     loopTime+Math.PI, axis: [0, 0, 1] }, scale: [1, -1, 1], color: [0.83, 0.67, 0.3]},
         { obj: worldObjects.fMuzzle, translation: [2120, 1770, 4550], rotation: { angle: loopTime+5*Math.PI/4, axis: [0, 0, 1] }, scale: [1, -1, 1], color: [0.83, 0.67, 0.3]},
         { obj: worldObjects.fMuzzle, translation: [2120, 1770, 4550], rotation: { angle: loopTime+3*Math.PI/2, axis: [0, 0, 1] }, scale: [1, -1, 1], color: [0.83, 0.67, 0.3]},
         { obj: worldObjects.fMuzzle, translation: [2120, 1770, 4550], rotation: { angle: loopTime+7*Math.PI/4, axis: [0, 0, 1] }, scale: [1, -1, 1], color: [0.83, 0.67, 0.3]},
         { obj: worldObjects.bullet,  translation: [2115, 1770, 4000], rotation: { angle:                    0, axis: [0, 1, 0] }, scale: [1, -1, 1], color: [0.8, 0.8, 0.8]},
     ];
-    
+
     objectsToRenderWithProgram2.forEach(({ obj, translation, rotation, scale, color}) => {
-        const modelMatrix = mat4.create();
-        mat4.translate(modelMatrix, modelMatrix, translation);
-        const normalMatrix = mat3.create();
-        mat3.normalFromMat4(normalMatrix, modelMatrix);
-        gl.uniformMatrix3fv(locations2.uniforms.normalMatrix, false, normalMatrix);
         gl.uniform3fv(locations2.uniforms.objectColor, color);    
         placeObj(gl, obj, translation, rotation, scale, locations2, 0);
     });
 }
+
+
+// { obj: worldObjects.gun,     translation: [120+wpnXOff, -130+wpnYOff, -450+wpnZOff], rotation: { angle:                    0, axis: [0, 1, 0] }, scale: [1, -1, 1], color: [0.83, 0.67, 0.3]},
+//         { obj: worldObjects.fMuzzle, translation: [120+wpnXOff, -130+wpnYOff, -450+wpnZOff], rotation: { angle:             loopTime, axis: [0, 0, 1] }, scale: [1, -1, 1], color: [0.83, 0.67, 0.3]},
+//         { obj: worldObjects.fMuzzle, translation: [120+wpnXOff, -130+wpnYOff, -450+wpnZOff], rotation: { angle:     loopTime+Math.PI, axis: [0, 0, 1] }, scale: [1, -1, 1], color: [0.83, 0.67, 0.3]},
+//         { obj: worldObjects.fMuzzle, translation: [120+wpnXOff, -130+wpnYOff, -450+wpnZOff], rotation: { angle:   loopTime+Math.PI/4, axis: [0, 0, 1] }, scale: [1, -1, 1], color: [0.83, 0.67, 0.3]},
+//         { obj: worldObjects.fMuzzle, translation: [120+wpnXOff, -130+wpnYOff, -450+wpnZOff], rotation: { angle:   loopTime+Math.PI/2, axis: [0, 0, 1] }, scale: [1, -1, 1], color: [0.83, 0.67, 0.3]},
+//         { obj: worldObjects.fMuzzle, translation: [120+wpnXOff, -130+wpnYOff, -450+wpnZOff], rotation: { angle: loopTime+3*Math.PI/4, axis: [0, 0, 1] }, scale: [1, -1, 1], color: [0.83, 0.67, 0.3]},
+//         { obj: worldObjects.fMuzzle, translation: [120+wpnXOff, -130+wpnYOff, -450+wpnZOff], rotation: { angle: loopTime+5*Math.PI/4, axis: [0, 0, 1] }, scale: [1, -1, 1], color: [0.83, 0.67, 0.3]},
+//         { obj: worldObjects.fMuzzle, translation: [120+wpnXOff, -130+wpnYOff, -450+wpnZOff], rotation: { angle: loopTime+3*Math.PI/2, axis: [0, 0, 1] }, scale: [1, -1, 1], color: [0.83, 0.67, 0.3]},
+//         { obj: worldObjects.fMuzzle, translation: [120+wpnXOff, -130+wpnYOff, -450+wpnZOff], rotation: { angle: loopTime+7*Math.PI/4, axis: [0, 0, 1] }, scale: [1, -1, 1], color: [0.83, 0.67, 0.3]},
+//         { obj: worldObjects.bullet,  translation: [115+wpnXOff, -130+wpnYOff, -1000+wpnZOff], rotation: { angle:                   0, axis: [0, 1, 0] }, scale: [1, -1, 1], color: [0.8, 0.8, 0.8]},
